c7ca8260fa1b9473d64026b22fe982421f9270dc
Staralalalalalok <firdausmustar@gmail.com>
2026-01-17 10:24:31 +0800

    test

--- src/data/csvParser.js
+++ src/data/csvParser.js
@@ -1,3 +1,6 @@
+// Use esm.sh to get a version of PapaParse that works with 'import'
+import Papa from 'https://esm.sh/papaparse@5.4.1';
+
 export class CsvParser {
   /**
    * Parses CSV text using PapaParse.
@@ -6,12 +9,20 @@ export class CsvParser {
    */
   static async parse(csvText) {
     return new Promise((resolve, reject) => {
-      PapaParse.parse(csvText, {
+      Papa.parse(csvText, {
         header: true,
         skipEmptyLines: true,
         complete: (results) => {
           if (results.errors.length) {
-            reject(new Error('CSV parsing errors: ' + JSON.stringify(results.errors)));
+            // Check if errors are actual blocking errors or just warnings
+            // PapaParse sometimes returns warnings in the errors array
+            const criticalErrors = results.errors.filter(e => e.type === 'Delimiter' || e.type === 'Quotes');
+
+            if (criticalErrors.length > 0) {
+              reject(new Error('CSV parsing errors: ' + JSON.stringify(criticalErrors)));
+            } else {
+              resolve(results.data);
+            }
           } else {
             resolve(results.data);
           }
